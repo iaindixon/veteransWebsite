7dd742ac845c553b61a74e70df0c01a114f06c37
Iain Dixon <iain@dixon.net>
2015-03-30 09:40:36 -0400

    Commit Various
    Added:
    - jQuery Librar 2.1.3
    - HTML Portal and footer div
    - Website Name
    - CSS links to match new HTML elements

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,22 @@
-function 
+function whichTransitionEvent(){
+    var t;
+    var el = document.getElementById('portal');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+}
+
+/* Listen for a transition! */
+var transitionEvent = whichTransitionEvent();
+transitionEvent && e.addEventListener(transitionEvent, function() {
+	
+});
