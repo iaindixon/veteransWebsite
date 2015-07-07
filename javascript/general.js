b4a2e6551c19f2a31363f34019f762e690c636e1
Iain Dixon <iain@dixon.net>
2015-07-07 10:05:00 -0400

    Descriptive Large Update 7/7/15
    Super update to head for Ryan <3

--- javascript/general.js
+++ javascript/general.js
@@ -1,22 +1,30 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
 function resizeIframe(obj) {
-    
+    "use strict";
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 }
 
-function changePage(link) {
-    "use strict";
-    var element = document.getElementById('myIframe').setAttribute('src', link.href);
-}
-
-//function loadContent(evt) {
-//    var e = evt.srcElement;
-//    var i = e.innerHTML;
-//    console.log("hi!", i);
-//    if (i === "Calendar") {
-//        resizeIframe(this);
-//    }
-//    else {
-//        this.style.height;
-//        resizeIframe(this);   
-//    }
-//}
+window.onload = function () {
+    'use strict';
+    $('#computer.row > a').click(function () {
+        $('#myIframe').css('height', '800px');
+        if ($(this).attr('href').substr(5) === 'events.html') {
+            window.console.log('Events');
+        } else if ($(this).attr('href').substr(5) === 'home.html') {
+            window.console.log('Home');
+        } else if ($(this).attr('href').substr(5) === 'info.html') {
+            window.console.log('Info');
+        } else if ($(this).attr('href').substr(5) === 'contact.html') {
+            window.console.log('Contact');
+        } else {
+            window.console.log('The page you have clicked on has not loaded.');
+        }
+        var source = $(this).attr('href').substr(5),
+            height = 0;
+        $.get('../html/' + source, function (i) {
+            var j = i;
+            window.console.log($(j).height());
+        });
+    });
+};
---
+++ javascript/test.js
@@ -0,0 +1,7 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+/*global var, pageName*/
+window.onload = function () {
+    'use strict';
+    window.console.log(pageName);
+};
