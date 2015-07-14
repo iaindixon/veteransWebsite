7ff0752e64d66ab673e4fd3ab894ab066ea96f9c
Iain Dixon <iain@dixon.net>
2015-07-14 11:21:05 -0400

    Javascript and CSS Update
    Updated everything - pending Ryan’s approval.

--- javascript/general.js
+++ javascript/general.js
@@ -1,30 +1,30 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
-function resizeIframe(obj) {
-    "use strict";
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+/*Creates the Moving bar when in iPhone mode*/
+function toggle() {
+    'use strict';
+    $('#iconMenu.toggleOn').click(function () {
+        $('#dropDown').animate({
+            left: '-285px'
+        }, 200);
+        $('body').animate({
+            left: '0px'
+        }, 200);
+        $('#iconMenu').removeClass('toggleOn').addClass('toggleOff');
+    });
+}
+function main() {
+    'use strict';
+    $('#iconMenu.toggleOff').click(function () {
+        $('#dropDown').animate({
+            left: '0px'
+        }, 200);
+        $('body').animate({
+            left: '285px'
+        }, 200);
+        $('#iconMenu.toggleOff').removeClass('toggleOff').addClass('toggleOn');
+        toggle();
+    });
 }
 
-window.onload = function () {
-    'use strict';
-    $('#computer.row > a').click(function () {
-        $('#myIframe').css('height', '800px');
-        if ($(this).attr('href').substr(5) === 'events.html') {
-            window.console.log('Events');
-        } else if ($(this).attr('href').substr(5) === 'home.html') {
-            window.console.log('Home');
-        } else if ($(this).attr('href').substr(5) === 'info.html') {
-            window.console.log('Info');
-        } else if ($(this).attr('href').substr(5) === 'contact.html') {
-            window.console.log('Contact');
-        } else {
-            window.console.log('The page you have clicked on has not loaded.');
-        }
-        var source = $(this).attr('href').substr(5),
-            height = 0;
-        $.get('../html/' + source, function (i) {
-            var j = i;
-            window.console.log($(j).height());
-        });
-    });
-};
+$(document).ready(main);
