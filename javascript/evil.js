0f7fb79879626f37b3fc079b73dbb651f391fe4c
Iain Dixon <iain@dixon.net>
2015-07-23 11:04:20 -0400

    PLS WORK
    GHAHHHAH

--- javascript/evil.js
+++ javascript/evil.js
@@ -1,14 +1,21 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
 $(document).ready(function () {
     'use strict';
-  $('a[href*=#]:not([href=#])').click(function () {if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 75
-        }, 1000);
-        return false;
-      }
+    var offset = 75;
+    if ($(window).width() < 568) {
+        offset = 50;
     }
-  });
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - offset
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
--- javascript/general.js
+++ javascript/general.js
@@ -1,11 +1,30 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 /*Creates the Moving bar when in iPhone mode*/
+function moveDown() {
+    'use strict';
+    var offset = 75;
+    if ($(window).width() < 568) {
+        offset = 50;
+    }
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - offset
+                }, 1000);
+                return false;
+            }
+        }
+    });
+}
 function main() {
     'use strict';
     var timing = 200;
     $('#iconMenu').click(function () {
-        if($('#iconMenu').hasClass('visible')) {
+        if ($('#iconMenu').hasClass('visible')) {
             $('#dropDown').animate({
                 left: '-285px'
             }, timing);
@@ -23,5 +42,8 @@ function main() {
         $('#iconMenu').toggleClass('visible');
     });
 }
-
-$(document).ready(main);
+$(document).ready(function () {
+    'use strict';
+    main();
+    moveDown();
+});
